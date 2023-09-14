2968bfbd90650c2b32ce86eab763a7be6155064d
NamKiHyeok <carpediem7490@gmail.com>
2023-09-14 14:35:06 +0900

    2023.09.14 Lecture: Controller의 입력

---
+++ src/main/java/kr/co/rland/web/controller/MenuController.java
@@ -0,0 +1,18 @@
+package kr.co.rland.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+	@RequestMapping("list")
+	public String list() {
+		return "/WEB-INF/view/menu/list.jsp";
+	}
+	
+	@RequestMapping("detail")
+	public String datail() {
+		return "/WEB-INF/view/menu/detail.jsp";
+	}
+}
